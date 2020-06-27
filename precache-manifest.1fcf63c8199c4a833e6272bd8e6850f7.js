2b9d1dbe12d588726b16d0197d9ace74e357009a
Oleg <oleg.irchishyn91@gmail.com>
2020-06-27 16:50:13 +0300

    Updates

---
+++ precache-manifest.1fcf63c8199c4a833e6272bd8e6850f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6870605710fa4d9df0936dd4a191ab",
+    "url": "/E-shop/index.html"
+  },
+  {
+    "revision": "51a63b4f0fae0ca1cdc2",
+    "url": "/E-shop/static/css/main.0a9e9d0b.chunk.css"
+  },
+  {
+    "revision": "87ad149396ed84f457a2",
+    "url": "/E-shop/static/js/2.9824367b.chunk.js"
+  },
+  {
+    "revision": "b42f36b5d572c611b888796d07d4a679",
+    "url": "/E-shop/static/js/2.9824367b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51a63b4f0fae0ca1cdc2",
+    "url": "/E-shop/static/js/main.088dc859.chunk.js"
+  },
+  {
+    "revision": "6158be008d9a9b3ee819",
+    "url": "/E-shop/static/js/runtime-main.461efeaa.js"
+  }
+]);
