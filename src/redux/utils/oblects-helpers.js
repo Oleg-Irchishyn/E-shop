2fb1524ba3acdcc05583887c9d502f0be5e3ee1b
Oleg <oleg.irchishyn91@gmail.com>
2020-06-29 23:16:20 +0300

    fixed a menu bug

--- src/redux/utils/oblects-helpers.js
+++ src/redux/utils/oblects-helpers.js
@@ -4,3 +4,8 @@ import React from 'react';
 export const createMenuItem = (name, active, method) => (
   <Menu.Item name={name} active={active} onClick={method} />
 )
+
+setTimeout(function () {
+  document.querySelector(".ui.secondary.menu a:nth-of-type(1").click();
+}, 50);
+
