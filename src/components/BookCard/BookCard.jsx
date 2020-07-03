23a1217de6d831d25b2ab3d82dab23bf0c362b4d
Oleg <oleg.irchishyn91@gmail.com>
2020-07-03 13:27:54 +0300

    Adding new styles to a project

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -5,30 +5,43 @@ import *as cartActions from './../../redux/reducers/cartReducer';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import './bookCard.scss';
 
 const BookCard = (book) => {
-  const { title, author, price, image, addBookToCart, addedCount } = book
+  const { title, author, price, image, addBookToCart, addedCount, rating } = book
+  let ratingCount = Math.ceil(rating);
+  let ratingStars = [];
+  for (let i = 1; i <= ratingCount; i++) {
+    ratingStars.push(i);
+  }
+
   return (
-    <Card>
+    <Card className="book-card">
       <Image src={image} wrapped ui={false} />
       <Card.Content>
-        <Card.Header>{title}</Card.Header>
+        <Card.Header className="book-card__header">{title}</Card.Header>
         <Card.Meta>
-          <span className='date'>{author}</span>
+          <span className='book-card__author'>{author}</span>
         </Card.Meta>
       </Card.Content>
-      <Card.Content extra>
+      <Card.Content extra className='book-card__extra-content'>
         <a>
           <Icon name='rub' />
           {price}
         </a>
+        <div className="book-card__rating-stars">
+          {ratingStars.map((s, index) =>
+            s = <span key={index} className="fa fa-star checked"></span>)
+          }
+        </div>
       </Card.Content>
-      <Button onClick={addBookToCart.bind(this, book)}>
+      <Button className="book-card__button" onClick={addBookToCart.bind(this, book)}>
         Add to Cart {addedCount > 0 && `(${addedCount})`}
       </Button>
     </Card>
   )
 }
+
 
 BookCard.propTypes = {
   book: PropTypes.object,
@@ -37,7 +50,8 @@ BookCard.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
-  image: PropTypes.string.isRequired
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
 }
 
 const mapStateToProps = ({ cart }, { id }) => ({
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 import 'semantic-ui-css/semantic.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css'
 
 ReactDOM.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
