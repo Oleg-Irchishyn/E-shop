d781737df10e350a0912cc2ca2ac64556c7ad28e
Oleg <oleg.irchishyn91@gmail.com>
2020-07-16 17:37:26 +0300

    updated package.json

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import 'semantic-ui-css/semantic.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-import './index.css'
+import './index.css';
 
 ReactDOM.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
